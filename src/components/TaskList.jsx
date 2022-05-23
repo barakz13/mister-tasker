676738dfdc509f98590dab08eed88c712bc94062
Barak Levav <levavbarak@gmail.com>
2022-05-23 22:28:43 +0300

    basic list rendering using data grid

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,5 +1,7 @@
 import { TaskPreview } from './TaskPreview';
 import { useEffect } from 'react';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { getDate } from '../services/util-service';
 
 export function TaskList({ tasks }) {
   useEffect(() => {
@@ -8,13 +10,32 @@ export function TaskList({ tasks }) {
     return () => {};
   }, []);
 
+  const rows = tasks.map((task) => ({
+    ...task,
+    createdAt: getDate(task.createdAt),
+  }));
+
+  const columns = [
+    { field: 'title', headerName: 'Title', width: 150 },
+    { field: 'status', headerName: 'Status', width: 150 },
+    { field: 'importance', headerName: 'Importance', width: 150 },
+    {
+      field: 'createdAt',
+      headerName: 'Created At',
+      width: 150,
+    },
+  ];
+
   return (
     <section className="task-list-section">
-      <ul className="task-list flex column">
+      {/* <ul className="task-list flex column">
         {tasks.map((task) => (
           <TaskPreview task={task} key={task.id} />
         ))}
-      </ul>
+      </ul> */}
+      <div style={{ height: 300, width: '50%', marginLeft: '25%' }}>
+        <DataGrid rows={rows} columns={columns} />
+      </div>
     </section>
   );
 }
